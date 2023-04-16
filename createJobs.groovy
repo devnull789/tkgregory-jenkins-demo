6dc4273dff6e87c5f42d4004e32c237429051258
Dev Null <devnull567@gmail.com>
2023-04-15 20:21:54 -0700

    pointed to my repos w tutorial code

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,16 @@
-pipelineJob('pipelineJob') {
+pipelineJob('theme-park-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/devnull789/tkgregory-spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
+
+
